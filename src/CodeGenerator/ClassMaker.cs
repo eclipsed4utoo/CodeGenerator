445c888d700deb22b80253edba34ea742e18d6b1
Ryan Alford <ryan@eclipsed4utoo.com>
2013-04-27 10:52:10 -0400

    initial commit

---
+++ src/CodeGenerator/ClassMaker.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace CodeGenerator
+{
+    public class ClassMaker
+    {
+        public static void MakeClass(string directory, string className, Dictionary<string, string> dic)
+        {
+            //if (className.EndsWith("s"))
+            //{
+            //    className = className.Substring(0, className.Length - 1);
+            //}
+            //else if (className.EndsWith("es"))
+            //{
+            //    className = className.Substring(0, className.Length - 2);
+            //}
+            //else if (className.EndsWith("ies"))
+            //{
+            //    className = className.Substring(0, className.Length - 3) + "y";
+            //}
+
+            using (TextWriter tw = new StreamWriter(string.Format("{0}\\{1}.cs", directory, className), false))
+            {
+                tw.WriteLine("using System;");
+                tw.WriteLine();
+                tw.WriteLine(string.Format("public class {0}", className));
+                tw.WriteLine("{");
+
+                foreach (KeyValuePair<string, string> s in dic)
+                {
+                    string type = string.Empty;
+                    string baseType = (s.Value.Contains("(")) ? s.Value.Substring(0, s.Value.IndexOf("(")) : s.Value;
+
+                    switch (baseType)
+                    {
+                        case "bigint":
+                            type = "Int64";
+                            break;
+
+                        case "int":
+                            type = "Int32";
+                            break;
+
+                        case "varbinary":
+                        case "rowversion":
+                        case "binary":
+                            type = "byte[]";
+                            break;
+
+                        case "bit":
+                            type = "bool";
+                            break;
+
+                        case "date":
+                        case "datetime":
+                        case "datetime2":
+                            type = "DateTime";
+                            break;
+
+                        case "smallmoney":
+                        case "numeric":
+                        case "money":
+                        case "decimal":
+                            type = "decimal";
+                            break;
+
+                        case "float":
+                            type = "double";
+                            break;
+
+                        case "real":
+                            type = "Single";
+                            break;
+
+                        case "smallint":
+                            type = "Int16";
+                            break;
+
+                        case "sql_variant":
+                            type = "object";
+                            break;
+
+                        case "time":
+                            type = "TimeSpan";
+                            break;
+
+                        case "tinyint":
+                            type = "byte";
+                            break;
+
+                        case "uniqueidentifier":
+                            type = "Guid";
+                            break;
+
+                        case "char":
+                        case "nchar":
+                        case "nvarchar":
+                        default:
+                            type = "string";
+                            break;
+                    }
+
+                    tw.WriteLine(string.Format("\tpublic {0} {1};", type, s.Key));
+                }
+
+                tw.WriteLine("}");
+            }
+        }
+    }
+}
---
+++ src/CodeGenerator/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace CodeGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string directory = string.Empty;
+            string server = string.Empty;
+            string database = string.Empty;
+            string userName = string.Empty;
+            string password = string.Empty;
+
+            foreach (string s in args)
+            {
+                if (s.Contains("/dir:"))
+                    directory = s.Substring(5);
+                else if (s.Contains("/s:"))
+                    server = s.Substring(3);
+                else if (s.Contains("/d:"))
+                    database = s.Substring(3);
+                else if (s.Contains("/u:"))
+                    userName = s.Substring(3);
+                else if (s.Contains("/p:"))
+                    password = s.Substring(3);
+            }
+
+            string connectionstring = string.Format("Data Source={0};Initial Catalog={1};User Id={2};Password={3}", server, database, userName, password);
+
+            DataTable dt = new DataTable();
+
+            using (SqlConnection cn = new SqlConnection(connectionstring))
+            {
+                cn.Open();
+                dt = cn.GetSchema("Tables");
+
+                foreach (DataRow row in dt.Rows)
+                {
+                    DataTable columns = new DataTable();
+                    string[] restrictions = new string[4]{ database, null, row[2].ToString(), null };
+                    columns = cn.GetSchema("Columns", restrictions);
+
+                    Dictionary<string, string> col = new Dictionary<string, string>();
+
+                    foreach (DataRow row2 in columns.Rows)
+                    {
+                        col.Add(row2[3].ToString(), row2[7].ToString());
+                    }
+
+                    ClassMaker.MakeClass(directory, row[2].ToString(), col);
+                }
+            }
+        }
+    }
+}
